0bc5365184cfb94f4a67bac944ea58fdf89b2444
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-12-23 19:03:44 +0100

    home globe image to eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,37 @@
 const now = String(Date.now())
 const htmlmin = require('html-minifier')
+const Image = require("@11ty/eleventy-img")
+
+async function imageShortcode(src, alt, sizes, imgClass) {
+    if(alt === undefined) {
+        // You bet we throw an error on missing alt (alt="" works okay)
+        throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+    }
+
+    let metadata = await Image(src, {
+        widths: [300, 600, 1200, 1800],
+        formats: ['webp', 'jpeg'],
+        urlPath: "/static/images/",
+        outputDir: "./_site/static/images/"
+    });
+
+    let lowsrc = metadata.jpeg[0];
+    let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+    return `<picture>
+        ${Object.values(metadata).map(imageFormat => {
+        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+        }).join("\n")}
+        <img
+            src="${lowsrc.url}"
+            width="${highsrc.width}"
+            height="${highsrc.height}"
+            alt="${alt}"
+            class="${imgClass}"
+            loading="lazy"
+            decoding="async">
+        </picture>`;
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/styles/tailwind.config.js')
@@ -14,9 +46,9 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/Camino-Media-Kit.zip')
 
-  eleventyConfig.addShortcode('version', function () {
-    return now
-  })
+  eleventyConfig.addShortcode('version', function () { return now })
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
 
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (
