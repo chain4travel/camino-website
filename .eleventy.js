12d5d2ec30a095f4fa4c587be19bf742e8305ae6
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-04-28 13:06:27 +0200

    fixes for images and z-index

--- .eleventy.js
+++ .eleventy.js
@@ -2,38 +2,39 @@ const now = String(Date.now())
 const htmlmin = require('html-minifier')
 const Image = require("@11ty/eleventy-img")
 
-async function imageShortcode(src, alt, pictureClass, imgClass, sizes = "100vw") {
-    if(alt === undefined) {
-        // You bet we throw an error on missing alt (alt="" works okay)
-        throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-    }
-
-    let metadata = await Image(src, {
-        widths: [160, 320, 640, 1280],
-        formats: ['avif', 'png'],
-        urlPath: "/static/images/",
-        outputDir: "./_site/static/images/"
-    });
-
-    let lowsrc = metadata.png[0];
-    let highsrc = metadata.png[metadata.png.length - 1];
-
-    return `<picture class="${pictureClass}">
-        ${Object.values(metadata).map(imageFormat => {
-        return `<source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-        }).join("\n")}
-        <img
-            src="${lowsrc.url}"
-            width="${highsrc.width}"
-            height="${highsrc.height}"
-            alt="${alt}"
-            class="${imgClass}"
-            loading="lazy"
-            decoding="async">
-        </picture>`;
-}
-
 module.exports = function (eleventyConfig) {
+
+  async function imageShortcode(src, alt, pictureClass, imgClass, sizes = "100vw") {
+      if(alt === undefined) {
+          // You bet we throw an error on missing alt (alt="" works okay)
+          throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+      }
+  
+      let metadata = await Image(src, {
+          widths: [300, 600, 1200],
+          formats: ['avif', 'png'],
+          outputDir: "./_site/static/images/",
+          urlPath: "/static/images/"
+      });
+  
+      let lowsrc = metadata.png[0];
+      let highsrc = metadata.png[metadata.png.length - 1];
+  
+      return `<picture class="${pictureClass}">
+          ${Object.values(metadata).map(imageFormat => {
+          return ` <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+          }).join("\n")}
+          <img
+              src="${lowsrc.url}"
+              width="${highsrc.width}"
+              height="${highsrc.height}"
+              alt="${alt}"
+              class="${imgClass}"
+              loading="lazy"
+              decoding="async">
+          </picture>`;
+  }
+
   eleventyConfig.addWatchTarget('./src/styles/tailwind.config.js')
   eleventyConfig.addWatchTarget('./src/styles/tailwind.css')
 
@@ -49,7 +50,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode('version', function () { return now })
 
-  eleventyConfig.addShortcode("image", imageShortcode)
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
 
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (
