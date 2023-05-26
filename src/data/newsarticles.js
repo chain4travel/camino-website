bc44f429d3b89ab526671410e5e7ca3f63e3b115
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-05-26 10:43:05 +0200

    updated news api endpoint

--- src/data/newsarticles.js
+++ src/data/newsarticles.js
@@ -1,5 +1,5 @@
 const Cache = require('@11ty/eleventy-cache-assets')
 
 module.exports = async function () {
-	return Cache('https://api.strapi.camino.network/news-articles?populate=*&pagination[limit]=-1', { type: 'json' })
+	return Cache('https://api.strapi.camino.network/news-article-caminos?sort=date%3Adesc&populate=*&pagination[limit]=-1', { type: 'json' })
 }
