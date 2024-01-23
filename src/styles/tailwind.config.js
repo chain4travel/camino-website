ab38fca955c8d07e675a8b2ba6c5a871fe409118
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-01-23 12:54:58 +0100

    fixes and updates

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -19,6 +19,30 @@ module.exports = {
             },
             animation: {
                 'spin-slow': 'spin 3s linear infinite',
+                'gradient-x':'gradient-x 5s ease infinite',
+                'gradient-xy':'gradient-xy 5s ease infinite',
+            },
+            keyframes: {
+                'gradient-x': {
+                    '0%, 100%': {
+                        'background-size':'200% 200%',
+                        'background-position': 'left center'
+                    },
+                    '50%': {
+                        'background-size':'200% 200%',
+                        'background-position': 'right center'
+                    }
+                },
+                'gradient-xy': {
+                    '0%, 100%': {
+                        'background-size':'400% 400%',
+                        'background-position': 'left center'
+                    },
+                    '50%': {
+                        'background-size':'200% 200%',
+                        'background-position': 'right center'
+                    }
+                }
             }
         },
         fontFamily: {
