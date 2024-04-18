a6ae563422d5ce666f7cd69343611fea2d042902
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-04-18 13:03:48 +0200

    Updated packages and fetching

--- src/data/newsarticles.js
+++ src/data/newsarticles.js
@@ -1,5 +1,12 @@
-const Cache = require('@11ty/eleventy-cache-assets')
+const axios = require('axios');
 
 module.exports = async function () {
-	return Cache('https://api.strapi.camino.network/news-article-caminos?sort=date%3Adesc&populate=*&pagination[limit]=-1', { type: 'json' })
-}
+	const url = 'https://api.strapi.camino.network/news-article-caminos?sort=date%3Adesc&populate=*&pagination[limit]=-1';
+	try {
+		const response = await axios.get(url);
+		return response.data;
+		} catch (error) {
+    console.error('Error fetching data:', error);
+    // Handle error here (e.g., throw error or return default data)
+	}
+};
